b19ba9185ed7573b3707067bd664081d518f3bbc
Itilon <ivanov.elect@gmail.com>
2020-08-02 22:15:42 +0300

    Connection string updated

--- app/config/port.config.js
+++ app/config/port.config.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 3000;
-const connectionString = 'mongodb://localhost/architecture-db';
+const connectionString = 'mongodb+srv://yivanov:yivanov@123@atika-design.z11ct.mongodb.net/test';
 
 module.exports = { port, connectionString };
